86c2f6baf19f1c0c1d7490d6df7abe33b66be0d9
ronghuijun <798406328@qq.com>
2019-09-12 13:26:30 +0800

    很丑的界面

---
+++ src/com/view/Main.java
@@ -0,0 +1,12 @@
+package com.view;
+
+public class Main {
+
+	private static AddressView addressview;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  addressview =new AddressView();
+		  addressview.view();
+	}
+
+}
